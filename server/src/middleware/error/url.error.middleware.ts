8eabf31ff1df440d7636da3872f53429341c2ef0
gabrielberlucci <gabrielberluccidev@proton.me>
2026-03-11 00:12:50 -0300

    refactor: bring all the logic of error handling to new function which validates all errors

---
+++ server/src/middleware/error/url.error.middleware.ts
@@ -0,0 +1,29 @@
+import { type NextFunction, type Response, type Request } from 'express';
+import { AppError } from 'src/error/Apperror.js';
+import z, { ZodError } from 'zod';
+
+export const errorMiddleware = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  if (err instanceof ZodError) {
+    const zodError = z.flattenError(err);
+
+    res.status(400).send({
+      message: 'An error has occurred',
+      error: zodError,
+    });
+    return;
+  }
+  if (err instanceof AppError) {
+    res.status(err.statusCode).send({
+      message: err.message,
+    });
+    return;
+  }
+  res.status(500).send({
+    message: 'Internal server error',
+  });
+};
